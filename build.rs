cde3fc4ffc9d5c42eac98c5d5da8b8da237e772d
Lachlan Deakin <ljdgit@gmail.com>
2023-11-06 21:18:48 +1100

    Switch to cmake for build

--- build.rs
+++ build.rs
@@ -1,55 +1,21 @@
-use std::fs;
-
-use cc::Build;
+fn define_bool(b: bool) -> &'static str {
+    if b {
+        "ON"
+    } else {
+        "OFF"
+    }
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-
-    let mut build = cc::Build::new();
-
-    let target_mscv = cfg!(target_env = "msvc");
-    let add_file = |builder: &mut Build, folder: &str| {
-        for entry in fs::read_dir(folder).unwrap() {
-            let path = entry.unwrap().path();
-            if let Some(extension) = path.extension() {
-                if extension == "c" || extension == "cpp" || (!target_mscv && extension == "S") {
-                    builder.file(path);
-                }
-            }
-        }
-    };
-
-    add_file(&mut build, "c-blosc/blosc");
-
-    if cfg!(feature = "lz4") {
-        add_file(&mut build, "c-blosc/internal-complibs/lz4-1.9.3");
-        build.include("c-blosc/internal-complibs/lz4-1.9.3");
-        build.define("HAVE_LZ4", None);
-    }
-    if cfg!(feature = "zlib") {
-        add_file(&mut build, "c-blosc/internal-complibs/zlib-1.2.11");
-        build.include("c-blosc/internal-complibs/zlib-1.2.11");
-        build.define("HAVE_ZLIB", None);
-    }
-    if cfg!(feature = "zstd") {
-        add_file(&mut build, "c-blosc/internal-complibs/zstd-1.5.2/common");
-        add_file(&mut build, "c-blosc/internal-complibs/zstd-1.5.2/compress");
-        add_file(
-            &mut build,
-            "c-blosc/internal-complibs/zstd-1.5.2/decompress",
-        );
-        add_file(
-            &mut build,
-            "c-blosc/internal-complibs/zstd-1.5.2/dictBuilder",
-        );
-        build.include("c-blosc/internal-complibs/zstd-1.5.2");
-        build.define("HAVE_ZSTD", None);
-    }
-
-    let linklib = if cfg!(target_env = "msvc") {
-        "libblosc"
-    } else {
-        "blosc"
-    };
-    build.compile(linklib);
+    let mut config = cmake::Config::new("c-blosc");
+    config.define("DEACTIVATE_LZ4", define_bool(!cfg!(feature = "lz4")));
+    config.define("DEACTIVATE_SNAPPY", define_bool(!cfg!(feature = "snappy")));
+    config.define("DEACTIVATE_ZLIB", define_bool(!cfg!(feature = "zlib")));
+    config.define("DEACTIVATE_ZSTD", define_bool(!cfg!(feature = "zstd")));
+    let dst = config.build();
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-search=native={}/lib64", dst.display());
+    println!("cargo:rustc-link-lib=static=blosc");
 }
