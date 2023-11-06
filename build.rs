a15c3b214192697f6832f308719727fa9424299b
Magnus Ulimoen <magnus@ulimoen.dev>
2023-11-06 23:30:20 +0100

    Update c-blosc to 1.21.5

--- build.rs
+++ build.rs
@@ -29,27 +29,27 @@ fn main() {
     }
 
     if cfg!(feature = "lz4") {
-        add_file(&mut build, "c-blosc/internal-complibs/lz4-1.9.3");
-        build.include("c-blosc/internal-complibs/lz4-1.9.3");
+        add_file(&mut build, "c-blosc/internal-complibs/lz4-1.9.4");
+        build.include("c-blosc/internal-complibs/lz4-1.9.4");
         build.define("HAVE_LZ4", None);
     }
     if cfg!(feature = "zlib") {
-        add_file(&mut build, "c-blosc/internal-complibs/zlib-1.2.11");
-        build.include("c-blosc/internal-complibs/zlib-1.2.11");
+        add_file(&mut build, "c-blosc/internal-complibs/zlib-1.2.13");
+        build.include("c-blosc/internal-complibs/zlib-1.2.13");
         build.define("HAVE_ZLIB", None);
     }
     if cfg!(feature = "zstd") {
-        add_file(&mut build, "c-blosc/internal-complibs/zstd-1.5.2/common");
-        add_file(&mut build, "c-blosc/internal-complibs/zstd-1.5.2/compress");
+        add_file(&mut build, "c-blosc/internal-complibs/zstd-1.5.5/common");
+        add_file(&mut build, "c-blosc/internal-complibs/zstd-1.5.5/compress");
         add_file(
             &mut build,
-            "c-blosc/internal-complibs/zstd-1.5.2/decompress",
+            "c-blosc/internal-complibs/zstd-1.5.5/decompress",
         );
         add_file(
             &mut build,
-            "c-blosc/internal-complibs/zstd-1.5.2/dictBuilder",
+            "c-blosc/internal-complibs/zstd-1.5.5/dictBuilder",
         );
-        build.include("c-blosc/internal-complibs/zstd-1.5.2");
+        build.include("c-blosc/internal-complibs/zstd-1.5.5");
         build.define("HAVE_ZSTD", None);
     }
 
